f9fbcedd28d6b6c0c1f4e83858c74578883dc46c
Clara Meirelles <clara_meirelles@hotmail.com>
2021-07-25 03:41:59 -0300

    formulário voluntários

--- Front/adoteja/src/Pages/ApplicationForm/ApplicationFormPage.js
+++ Front/adoteja/src/Pages/ApplicationForm/ApplicationFormPage.js
@@ -1,17 +1,14 @@
-import { useForm } from "../../Hooks/useForm"
+import { ApplicationFormContainer } from "./styled"
+import ApplicationForm from "./ApplicationForm"
 
 export default function ApplicationFormPage() {
-    const { form, handleInput, clearInputs } = useForm()
-    const submitForm = (e) => {
-        e.preventDefault()
 
-    }
     return (
-        <>
-        FORMULARIO
-        <form onSubmit={submitForm}>
-            
-        </form>
-        </>
+        <ApplicationFormContainer>
+            <h1>Formulário de inscrição para voluntário</h1>
+            <p>Antes de qualquer coisa, muito obrigada por querer nos ajudar a melhorar a vida dos pets de Mogi das Cruzes!</p>
+            <p>Agora, precisamos de algumas informações sobre você, tudo bem?</p>
+            <ApplicationForm />
+        </ApplicationFormContainer>
     )
 }
