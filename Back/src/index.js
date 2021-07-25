440c79073c63471dde113c78511437344797e722
Clara Meirelles <clara_meirelles@hotmail.com>
2021-07-25 01:48:32 -0300

    teste

---
+++ Back/src/index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const rotas = require('./rotas');
+const cors = require('cors');
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+app.use(rotas);
+
+console.log('Servidor ativo!')
+
+app.listen(3000);
