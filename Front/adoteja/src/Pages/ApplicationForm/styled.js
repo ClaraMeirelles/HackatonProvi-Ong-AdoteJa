4fdabc414384e43cdc2c9eeef714f9770eaf1708
Clara Meirelles <clara_meirelles@hotmail.com>
2021-07-24 23:20:15 -0300

    criação formulário de voluntários

---
+++ Front/adoteja/src/Pages/ApplicationForm/styled.js
@@ -0,0 +1,17 @@
+import styled from "styled-components";
+
+export const ApplicationFormContainer = styled.section`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    padding: 16px;
+    form{
+        display: flex;
+        flex-direction: column;
+        width: 50vw;
+        button{
+            margin-top: 24px;
+        }
+    }
+`
